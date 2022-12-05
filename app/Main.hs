493b687ec20c16f57efb2cc0f2bbac249ef3e3b1
Michael Lazear <michaellazear92@gmail.com>
2022-12-04 20:45:14 -0800

    Day 4

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,12 @@ module Main where
 import qualified Day1 (run)
 import qualified Day2 (run)
 import qualified Day3 (run)
+import qualified Day4 (run)
 
 main :: IO ()
 main = do
   putStrLn "Advent of Code 2022"
   -- Day1.run "data/day1.txt"
   -- Day2.run "data/day2.txt"
-  Day3.run "data/day3.txt"
+  -- Day3.run "data/day3.txt"
+  Day4.run "data/day4.txt"
---
+++ src/Day4.hs
@@ -0,0 +1,40 @@
+module Day4(run) where
+
+import Text.Read
+import Data.List
+import qualified Data.Char as Char
+
+
+parse :: String -> IO [String]
+parse = fmap lines . readFile
+
+split :: (Char -> Bool) -> String -> [String]
+split f s =
+    case dropWhile f s 
+      of "" -> []
+         s' -> w : split f s''
+            where (w, s'') = break f s'
+
+toSections :: String -> [Int]
+toSections = map read . split (not . Char.isDigit)
+
+overlap :: (Bool -> Bool -> Bool) -> [Int] -> Bool
+overlap op [a, b, c, d] = overlap' (a, b) (c, d)
+    where
+        overlap' (a, b) (c, d) 
+            | (a >= c && a <= d) `op` (b >= c && b <= d) = True
+            | (c >= a && c <= b) `op` (d >= a && d <= b) = True
+            | otherwise = False
+overlap _ _ = False
+
+part1 :: [String] -> Int
+part1 = length . filter id . map (overlap (&&) . toSections)
+
+part2 :: [String] -> Int
+part2 = length . filter id . map (overlap (||) . toSections)
+
+run :: String -> IO ()
+run input = do
+    lines <- parse input
+    print $ part1 lines
+    print $ part2 lines
